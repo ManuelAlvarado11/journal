b8f39d46e7a69c3ad197ec402c0a23992bdc1458
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-05 15:31:51 -0600

    EntryListComponent

--- src/modules/daybook/components/EntryListComponent.vue
+++ src/modules/daybook/components/EntryListComponent.vue
@@ -1,7 +1,20 @@
 <template>
-  <h1>EntryList</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text" class="form-control" placeholder="Buscar entrada" />
+    </div>
+    <div class="entry-scrollarea">
+      <h2 v-for="item in 100" :key="item">Entry</h2>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+}
+</style>
