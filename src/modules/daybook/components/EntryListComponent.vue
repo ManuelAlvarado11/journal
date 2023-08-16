1f365964f4a3d191221e1fabc820ed5b082924a0
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-08-16 15:13:24 -0600

    :sparkles:Add favicon al proyecto y estilos EntryList

--- src/modules/daybook/components/EntryListComponent.vue
+++ src/modules/daybook/components/EntryListComponent.vue
@@ -4,7 +4,7 @@
       <input type="text" class="form-control" placeholder="Buscar entrada" />
     </div>
     <div class="entry-scrollarea">
-      <h2 v-for="item in 100" :key="item">Entry</h2>
+      <h2 v-for="item in 110" :key="item">Entry</h2>
     </div>
   </div>
 </template>
@@ -14,7 +14,16 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+input {
+  height: 50px;
+}
 .entry-list-container {
   border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+
+.entry-scrollarea {
+  height: calc(100vh - 120px);
+  overflow: scroll;
 }
 </style>
